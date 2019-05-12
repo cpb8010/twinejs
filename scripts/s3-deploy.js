3166b4c8c4c70ada4d00ec0de39efdc084191cdf
cpb8010 <cpb8010@gmail.com>
2019-05-12 12:27:17 -0500

    Use new build path

--- scripts/s3-deploy.js
+++ scripts/s3-deploy.js
@@ -12,7 +12,7 @@ const key_prefix = process.env.BUCKET_PATH || process.argv[3];
 const s3 = new S3({
 	accessKeyId: process.env.AWS_ACCESS_KEY,
 	secretAccessKey: process.env.AWS_SECRET_KEY});
-const build_folder = process.env.BUILD_FOLDER || "./build";
+const build_folder = process.env.BUILD_FOLDER || "./dist/web-cdn";
 const files = walkSync(build_folder, { directories: false });
 
 files.forEach(function (fileName) {
